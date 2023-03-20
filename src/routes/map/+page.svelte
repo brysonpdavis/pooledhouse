1fcc9b72f5525081b07234ab851d61f755a09e2a
brysonpdavis <brysonpd@gmail.com>
2023-03-20 09:59:59 -0400

    Refactor the GoogleMap component to only render on the client-side in +page.svelte

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import GoogleMap from '$lib/components/GoogleMap.svelte';
 	import type { PageData } from './$types';
-	export let data: PageData
+	import { browser } from '$app/environment';
+	export let data: PageData;
 </script>
 
 <div class="h-96 w-full">
-	<GoogleMap places={data.places} />
+	{#if browser}
+		<GoogleMap places={data.places} />
+	{/if}
 </div>
