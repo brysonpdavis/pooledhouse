81f688b312b6b450b302a58772a90f7fcd6d2dfc
brysonpdavis <brysonpd@gmail.com>
2023-04-03 10:15:29 -0400

    fix button styling on splash page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 	<div class="flex flex-col lg:flex-row">
 		<div class="card-bordered card m-4  p-4 lg:w-1/3">
-			<div class="flex flex-row justify-between gap-4">
+			<div class="flex-grow flex flex-row justify-between gap-4">
 				<div class="flex-grow">
 					<div class="card-title">looking for a new gig? just curious?</div>
 					<p>
@@ -11,31 +11,36 @@
 						workplaces in the city
 					</p>
 				</div>
-				<a class="hover:btn-primary-focus btn-primary btn btn-lg h-full" href="/explore">explore</a>
+				<a class="hover:btn-primary-focus btn-primary btn-lg btn h-full" href="/explore">explore</a>
 			</div>
 		</div>
 		<div class="card-bordered card m-4 p-4 lg:w-1/3">
-			<div class="flex justify-between gap-4 flex-grow flex-row lg:flex-row-reverse">
-				<a class="hover:btn-primary-focus btn-primary btn btn-lg h-full" href="/auth/register">register</a>
+			<div class="flex flex-grow flex-row justify-between gap-4 lg:flex-row-reverse">
+				<a class="hover:btn-primary-focus btn-primary btn-lg btn h-full" href="/auth/register"
+					>register</a
+				>
 				<div class="flex-grow">
 					<div class="card-title justify-end">share your experience</div>
 					<p>
-						after registering as a verified restaurant worker, you can verify your friends or coworkers, too
+						after registering as a verified restaurant worker, you can verify your friends or
+						coworkers, too
 					</p>
 				</div>
 			</div>
 		</div>
 		<div class="card-bordered card m-4  p-4 lg:w-1/3">
-			<div class="flex flex-row justify-between gap-4">
+			<div class="flex flex-grow flex-row justify-between gap-4">
 				<div class="flex-grow">
 					<div class="card-title">share your perspective on your own workplace</div>
 					<p>
-						let everyone know what it's really like at behind the curtain at your workplace, anonymously
+						let everyone know what it's really like at behind the curtain at your workplace,
+						anonymously
 					</p>
 				</div>
-				<a class="hover:btn-primary-focus btn-primary btn btn-lg h-full" href="/explore">contribute</a>
+				<a class="hover:btn-primary-focus btn-primary btn-lg btn h-full" href="/explore"
+					>contribute</a
+				>
 			</div>
 		</div>
 	</div>
 </div>
-
