e12b55e9c9360eb5ebcf48d3548404ad52eb3fb4
brysonpdavis <brysonpd@gmail.com>
2023-06-07 05:27:02 -0400

    center login and signup pages

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -14,45 +14,47 @@
 	}
 </script>
 
-<h2>would you like to login by email or phone number?</h2>
+<div class="w-fit m-auto">
+	<h2>would you like to login by email or phone number?</h2>
 
-<form
-	on:submit={async (event) => {
-		event.preventDefault();
-		await handleSubmit();
-	}}
-	class="flex flex-col"
->
-	<!-- svelte-ignore a11y-autofocus -->
-	<div class="input-group">
-		{#if loginMethod === 'email'}
-			<input
-				required
-				autofocus
-				type="email"
-				name="email"
-				placeholder="email"
-				bind:value={identifier}
-				class="input-bordered input-primary input w-full max-w-lg"
-			/>
-		{:else}
-			<input
-				disabled
-				required
-				autofocus
-				type="tel"
-				name="phone"
-				placeholder="phone login is currently a work in progress"
-				bind:value={identifier}
-				class="input-bordered input-primary input w-full max-w-lg"
-			/>
-		{/if}
-		<select bind:value={loginMethod} class="select-bordered select">
-			<option value="email">email</option>
-			<option value="phone">phone</option>
-		</select>
-	</div>
-	<button type="submit" class="btn-secondary btn-outline btn mt-4 w-full max-w-lg">
-		log in!
-	</button>
-</form>
+	<form
+		on:submit={async (event) => {
+			event.preventDefault();
+			await handleSubmit();
+		}}
+		class="flex flex-col"
+	>
+		<!-- svelte-ignore a11y-autofocus -->
+		<div class="input-group">
+			{#if loginMethod === 'email'}
+				<input
+					required
+					autofocus
+					type="email"
+					name="email"
+					placeholder="email"
+					bind:value={identifier}
+					class="input-bordered input-primary input w-full max-w-lg"
+				/>
+			{:else}
+				<input
+					disabled
+					required
+					autofocus
+					type="tel"
+					name="phone"
+					placeholder="phone login is currently a work in progress"
+					bind:value={identifier}
+					class="input-bordered input-primary input w-full max-w-lg"
+				/>
+			{/if}
+			<select bind:value={loginMethod} class="select-bordered select">
+				<option value="email">email</option>
+				<option value="phone">phone</option>
+			</select>
+		</div>
+		<button type="submit" class="btn-outline btn-secondary btn mt-4 w-full">
+			log in!
+		</button>
+	</form>
+</div>
